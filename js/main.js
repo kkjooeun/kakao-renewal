307b75812c00c6362ce40d4f307b7af48d0380d7
kkjooeun <zero5309405@gmail.com>
2020-01-02 20:59:34 +0900

    이미지 수정 및 내용변경 완료

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 //mb-top부분
 var mbNavi = [
-	"BRAND",
-	"NEW",
-	"GIFT",
-	"FOR CAFE",
-	"GLASSWARE",
-	"KITCHEN",
-	"HOME DECO",
-	"FURNITURE",
-	"LIGHTING",
-	"OFFICE",
-	"CRAFT",
-	"COLLECT",
-	"STILL-LIFE"
+	"전체",
+	"테마 기획전",
+	"토이",
+	"리빙",
+	"잡화",
+	"문구",
+	"의류",
+	"파자마",
+	"여행/레져",
+	"생활테크",
+	"폰 액세서리",
+	"식품"
 ];
 var html = '';
 var scTop = 0;
@@ -33,7 +32,7 @@ $(".mb-navi").mouseenter(function () {
 	$(this).css("color", "#fff");
 });
 $(".mb-navi").mouseleave(function () {
-	$(this).css("color", "#ddd");
+	$(this).css("color", "#555");
 });
 
 // 아이콘 모양 바꾸기, 아이콘클릭
@@ -148,23 +147,13 @@ $(".navi-tit").mouseleave(function () {
 // top-bans부분
 
 var bans = [{
-	src: "../img/glassware_main_pc.jpg"
+	src: "../img/ban1.png"
+},{
+	src: "../img/ban2.png"
 }, {
-	src: "../img/pierre_Jeanneret_pc.jpg"
+	src: "../img/ban3.png"
 }, {
-	src: "../img/plyworks_main_pc_final.jpg"
-}, {
-	src: "../img/2019_pc_main_2.jpg"
-}, {
-	src: "../img/Brand_Driade_Main_pc_최종2.jpg"
-}, {
-	src: "../img/Ichendorf_color_pc.jpg"
-}, {
-	src: "../img/chapter1seoul_plustanpromotion_main_pc.jpg"
-}, {
-	src: "../img/paolac_main_pc.jpg"
-}, {
-	src: "../img/swissmobilia_main_pc_3.jpg"
+	src: "../img/ban4.png"
 }];
 
 
@@ -235,52 +224,52 @@ $(".pager > li").click(function () {
 
 //best-item 부분
 var bestItem = [{
-		src: "../img/8b170d964a670af05cfefa19f45f0b8c.jpg",
-		tit: "7월 CHAPTER1 DAY"
+		src: "../img/best1.jpg",
+		tit: "찍찍, 치즈프렌즈"
 	},
 	{
-		src: "../img/3123b90a5001a1ed016bb260c6c7dfba.jpg",
-		tit: "SUMMER - GLASSWARE"
+		src: "../img/best2.jpg",
+		tit: "어서와, 2020년은 처음이지?"
 	},
 	{
-		src: "../img/34f5ada22bef7c292e1bba5aab28b0e7.jpg",
-		tit: "SUMMER - FAN"
+		src: "../img/best3.jpg",
+		tit: "존재감 뿜뿜 <br> 귀여움도 사이즈도 자이언트"
 	},
 	{
-		src: "../img/2434abbeea105142be3954f660df951b.jpg",
-		tit: "CHAPTER1 SELECT<br>BEST ITEM"
+		src: "../img/best4.jpg",
+		tit: "보들보들 포근포근 <br> 폼폼프렌즈의 겨울이야기"
 	},
 	{
-		src: "../img/1f712d26c3a0d34e66ce99dd84091b77.jpg",
-		tit: "CHAPTER1 COLLECT<br>BEST ITEM"
+		src: "../img/best5.jpg",
+		tit: "프렌즈템 선물하고 <br> 센스쟁이 되기"
 	},
 	{
-		src: "../img/64049d871e7b6694a30ad67e1b545a70.jpg",
-		tit: "CHAPTER1 EDIT<br>BEST ITEM"
+		src: "../img/best6.jpg",
+		tit: "어서와, 2020년은 처음이지?"
 	},
 	{
-		src: "../img/f1d119a0ff277d4d3471932806890214.jpg",
-		tit: "WYU BUBBLE CUP SET (2PCS) <br>&#8361; 50,000"
+		src: "../img/best7.jpg",
+		tit: "찍찍, 치즈프렌즈"
 	},
 	{
-		src: "../img/3281_shop1_650612.jpg",
-		tit: "BLACK BOX OFFICE TRAY <br>&#8361; 38,000"
+		src: "../img/best8.jpg",
+		tit: "프렌즈템 선물하고 <br> 센스쟁이 되기"
 	},
 	{
-		src: "../img/795_shop1_894818.jpg",
-		tit: "TILL UMBRELLA STAND - BLACK <br>&#8361; 198,000"
+		src: "../img/best9.jpg",
+		tit: "우리 붕붕이, 좋은거 다해"
 	},
 	{
-		src: "../img/2f0f460d141c82a57663931455f3d662.jpg",
-		tit: "TRE COFFEE TABLE - WHITH <br>&#8361; 320,000"
+		src: "../img/best10.jpg",
+		tit: "찍찍, 치즈프렌즈"
 	},
 	{
-		src: "../img/3340_shop1_578880.jpg",
-		tit: "[주문 후 4개월 소요] <br> COMFORTABLE SOFA ROUND <br> 3SEATER_BROWN <br>&#8361; 1,980,000"
+		src: "../img/best11.jpg",
+		tit: "자이언트 라이언이 다 하는 <br> 내 방 무드"
 	},
 	{
-		src: "../img/2a253eda57540c928f3987b8488daf47.jpg",
-		tit: "ARNOLD CIRCUS STOOL, <br>PISTACHIO GREEN<br>&#8361; 220,000"
+		src: "../img/best12.jpg",
+		tit: "보들보들 포근포근 <br> 폼폼프렌즈의 겨울이야기"
 	}
 ];
 
@@ -306,124 +295,124 @@ $(".best-item").mouseleave(function () {
 
 // new부분
 var newArrival = [{
-		src: "../img/965d07d15a5f4f98f69d7ddaae16ef2b.jpg",
-		tit: "LINEA SOFA TABLE - WHITE",
-		price: "&#8361;650,000",
+		src: "../img/new1.jpg",
+		tit: "칫솔살균기 리틀라이언",
+		price: "&#8361;25,000",
 		event: ""
 	},
 	{
-		src: "../img/5a1991e0b5a8c2fbe3278e3749694129.jpg",
-		tit: "LINEA SOFA TABLE - BLACK",
-		price: "&#8361;650,000",
+		src: "../img/new2.jpg",
+		tit: "칫솔살균기 리틀어피치",
+		price: "&#8361;25,000",
 		event: ""
 	},
 	{
-		src: "../img/ac31fc1fb8ce30a94caed592531ac269.jpg",
+		src: "../img/new3.jpg",
 		tit: "LINEA SIDE BOARD - WALNUT",
 		price: "&#8361;2,300,000",
 		event: ""
 	},
 	{
-		src: "../img/df6a346003b483731e9d3cdae226ed3a.jpg",
-		tit: "COOL MESH MAT",
-		price: "&#8361;78,000",
+		src: "../img/new4.jpg",
+		tit: "화끈라면_라이언",
+		price: "&#8361;1,900",
 		event: ""
 	},
 	{
-		src: "../img/1fa0bf9ab1053d6540dfce967bf763ab.jpg",
-		tit: "BAMBOO SEERSUCKER BEDDING (홑겹)",
-		price: "&#8361;69,000",
+		src: "../img/new5.jpg",
+		tit: "봉골라면_어피치",
+		price: "&#8361;1,900",
 		event: ""
 	},
 	{
-		src: "../img/a6c3cae523a841911ab9bab90c464661.jpg",
-		tit: "MOLE ARMCHAIR BK BEECH",
-		price: "",
-		event: "[주문 후 4개월 소요]"
+		src: "../img/new6.jpg",
+		tit: "주차번호판_리틀라이언",
+		price: "&#8361;10,000",
+		event: "[무료배송 이벤트]"
 	},
 	{
-		src: "../img/93a1edfd9697391de9534f4f184b2a8d.jpg",
-		tit: "MOLE ARMCHAIR BR IMBUIA",
-		price: "",
-		event: "[주문 후 4개월 소요]"
+		src: "../img/new7.jpg",
+		tit: "주차번호판_리틀어피치",
+		price: "&#8361;10,000",
+		event: "[무료배송 이벤트]"
 	},
 	{
-		src: "../img/bc1b16cf06d8ee5591c7e5d6ab46669b.jpg",
-		tit: "민자 꼬리 컵",
-		price: "&#8361;45,000",
-		event: "[CHAPTER1 EDIT]"
+		src: "../img/new8.jpg",
+		tit: "리틀라이언 피규어디퓨저",
+		price: "&#8361;24,000",
+		event: "[특별세일중]"
 	},
 	{
-		src: "../img/0d89487d199714d753ccac36641b5116.jpg",
-		tit: "GLASS 4301 SET (2PCS)",
-		price: "&#8361;89,000",
+		src: "../img/new9.jpg",
+		tit: "리틀어피치 피규어디퓨저",
+		price: "&#8361;24,000",
+		event: "[특별세일중]"
+	},
+	{
+		src: "../img/new10.jpg",
+		tit: "지퍼백 10P Set-어피치",
+		price: "&#8361;7,000",
 		event: ""
 	},
 	{
-		src: "../img/964266c66e6525a3bcda32c21d60c1d2.jpg",
-		tit: "PEBBLE MIRROR",
-		price: "&#8361;190,000",
+		src: "../img/new11.jpg",
+		tit: "지퍼백 10P Set-라이언",
+		price: "&#8361;7,000",
 		event: ""
 	},
 	{
-		src: "../img/6d80b62585addc8d34cc895badd224de.jpg",
-		tit: "INCENSE STICK INCENSE BURBS",
-		price: "&#8361;25,000",
+		src: "../img/new12.jpg",
+		tit: "치즈 동전지갑 라이언",
+		price: "&#8361;13,000",
 		event: ""
 	},
 	{
-		src: "../img/46926a308e3b1ffc8037a640ce6e9b03.jpg",
-		tit: "TULIP WINE GOBLET",
+		src: "../img/new13.jpg",
+		tit: "치즈 동전지갑 어피치",
+		price: "&#8361;13,000",
+		event: ""
+	},
+	{
+		src: "../img/new14.jpg",
+		tit: "치즈 에어팟케이스 라이언",
+		price: "&#8361;19,000",
+		event: ""
+	},
+	{
+		src: "../img/new15.jpg",
+		tit: "치즈 케이블보호캡 라이언",
+		price: "&#8361;12,000",
+		event: ""
+	},
+	{
+		src: "../img/new16.jpg",
+		tit: "치즈 케이블보호캡 어피치",
+		price: "&#8361;12,000",
+		event: ""
+	},
+	{
+		src: "../img/new17.jpg",
+		tit: "치즈_프렌즈코인초콜릿세트",
+		price: "&#8361;14,000",
+		event: ""
+	},
+	{
+		src: "../img/new18.jpg",
+		tit: "글리터 케이스_리틀라이언",
+		price: "&#8361;15,000",
+		event: ""
+	},
+	{
+		src: "../img/new19.jpg",
+		tit: "글리터 케이스_리틀어피치",
+		price: "&#8361;15,000",
+		event: ""
+	},
+	{
+		src: "../img/new20.jpg",
+		tit: "치즈_빌리지 브릭피규어",
 		price: "&#8361;49,000",
-		event: ""
-	},
-	{
-		src: "../img/1be017dcb0b8207e3a0d3462f2a93f5a.jpg",
-		tit: "TULIP CHAMPAGNE GOBLET",
-		price: "&#8361;59,000",
-		event: ""
-	},
-	{
-		src: "../img/9a52fb37688faad8bd341ed818d20981.png",
-		tit: "APRON S - APPLE",
-		price: "&#8361;58,000",
-		event: ""
-	},
-	{
-		src: "../img/4ec3e0ec866336fef9acf553a45313ea.jpg",
-		tit: "CUTLERY 3001 SET",
-		price: "&#8361;69,000",
-		event: ""
-	},
-	{
-		src: "../img/a3dc0002737f6fcf3fef3ee62612bc24.jpg",
-		tit: "HERBARIUM - GREEN FOREST",
-		price: "&#8361;32,000",
-		event: ""
-	},
-	{
-		src: "../img/9c7ce5dec1783ddb3c1f4c55bd2eff59.jpg",
-		tit: "USEFUL SHAPE MIDDLE STOOL DEEP GRREN",
-		price: "&#8361;286,000",
-		event: ""
-	},
-	{
-		src: "../img/3ff6ae98beed1c9ac75cc62cfea265f6.jpg",
-		tit: "USEFUL SHAPE LOW STOOL, DEEP RED",
-		price: "&#8361;253,000",
-		event: ""
-	},
-	{
-		src: "../img/df6a346003b483731e9d3cdae226ed3a.jpg",
-		tit: "MARBLE APPLE - WHITE",
-		price: "&#8361;34,000",
-		event: ""
-	},
-	{
-		src: "../img/ad1048b9c1d645e092c928728ecf7f89.jpg",
-		tit: "TRIPOD WATER GLASS",
-		price: "&#8361;78,000",
-		event: "[주문 후 3개월 소요예정]"
+		event: "[온라인 단독 한정판매]"
 	}
 ];
 
